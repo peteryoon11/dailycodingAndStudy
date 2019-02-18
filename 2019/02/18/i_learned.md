4e38359b166752f6d1a0bfd232ee48635cb0757e
peteryoon11 <yjisub11@gmail.com>
2019-02-18 20:48:13 +0900

    add content for wish book and reason

---
+++ 2019/02/18/i_learned.md
@@ -0,0 +1,26 @@
+## wish book list 
+
+스프링 부트로 배우는 자바 웹 개발
+
+출처: <http://www.yes24.com/Product/Goods/61793313> 
+
+
+이펙티브 자바 Effective Java 3/E
+
+출처: <http://www.yes24.com/Product/Goods/65551284> 
+
+CentOS 리눅스 구축관리 실무
+
+출처: <http://www.yes24.com/Product/Goods/24843269> 
+
+CentOS 7 예비학교 예비 인프라 엔지니어를 위한 과외 수업
+
+출처: <http://www.yes24.com/Product/Goods/55161956> 
+
+-> 스프링  과 centos 에 대한 책을 구입하고 싶어져서 알아보고 있다. 
+
+1. 기존에는 centos 6.9를 썼는데 현재 회사는 centos 7 으로 넘어가 있어서 여러가지 바뀐 부분들에 대해서 알아두면 좋을거 같다. 
+
+2. java 8, 9  의 기능을 좀 더 정확하게 쓰고 싶어서 
+
+3. spring boot 의 내용을 좀더 자세하게 배우고 싶어서 
---
+++ 2019/02/18/testCode/golang_batchJob/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+import "time"
+
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+func main() {
+	//In order to use our pool of workers we need to send them work and collect their results. We make 2 channels for this.
+
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
+	//This starts up 3 workers, initially blocked because there are no jobs yet.
+
+	for w := 1; w <= 2; w++ {
+		go worker(w, jobs, results)
+	}
+	//Here we send 5 jobs and then close that channel to indicate that’s all the work we have.
+
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+	//Finally we collect all the results of the work.
+
+	for a := 1; a <= 5; a++ {
+		<-results
+	}
+}
