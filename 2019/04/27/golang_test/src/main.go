e107ee8efdbcbf6e75282e26695e33f1d8482367
peteryoon11 <yjisub11@gmail.com>
2019-04-27 15:02:20 +0900

    add content for today

---
+++ 2019/04/27/golang_test/src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+)
+
+func main() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	tempDir := filepath.Join("./ttt", "temp")
+	fmt.Println(tempDir)
+
+	err := os.Rename("./test", "./test2")
+	if err != nil {
+		fmt.Println(err)
+	}
+	r, err := zip.OpenReader("./test.zip")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, file := range r.File {
+		rc, err := file.Open()
+		if err != nil {
+			//return err
+			fmt.Println(err)
+		}
+	}
+}
