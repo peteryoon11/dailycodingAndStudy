6dbaa19fda80e3395c89cf8c23dce4dc4250a579
peteryoon11 <yjisub11@gmail.com>
2019-10-03 19:54:06 +0900

    add content for today

---
+++ 2019/10/03/exCode/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("test")
+
+	a := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(a)
+}
+
+func shift([]int array, int count) {
+		fmt.Println()
+}
