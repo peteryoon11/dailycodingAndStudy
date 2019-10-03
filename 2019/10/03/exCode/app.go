0d7c5072db831ddf5984f5bf20d2e731ec00981e
peteryoon11 <yjisub11@gmail.com>
2019-10-03 19:56:03 +0900

    add content for today

--- 2019/10/03/exCode/app.go
+++ 2019/10/03/exCode/app.go
@@ -8,10 +8,12 @@ func main() {
 	fmt.Println("test")
 
 	a := []int{1, 2, 3, 4, 5}
-
+	b := 2
 	fmt.Println(a)
+	shift(a, b)
 }
 
-func shift([]int array, int count) {
-		fmt.Println()
+func shift(array []int, count int) {
+	fmt.Println("array ", array)
+	fmt.Println("count ", count)
 }
