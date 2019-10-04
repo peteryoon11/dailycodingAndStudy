4fe37635fcec4e0b8d9e4e9a2cfd92ccc179bd2d
peteryoon11 <yjisub11@gmail.com>
2019-10-04 18:31:58 +0900

    add content for today

--- 2019/10/04/exCode/app.go
+++ 2019/10/04/exCode/app.go
@@ -10,10 +10,40 @@ func main() {
 	a := []int{1, 2, 3, 4, 5}
 	b := 2
 	fmt.Println(a)
-	shift(a, b)
+	fmt.Println(shift(a, b))
+	/*
+		1 2 3 4 5  , 2
+
+		=> 3 4 5 1 2
+
+	*/
+
+	// f1 := []string{"apple", "banana", "tomato"}
+	// f2 := []string{"grape", "strawberry"}
+	// f3 := append(f1, f2) // 이어 붙이기
 }
 
-func shift(array []int, count int) {
+func shift(array []int, count int) []int {
+
+	fmt.Println("pre array ", array[:count])
+	fmt.Println("post array ", array[count:])
+	// preArray := array[:count]
+	// postArray := array[count:]
 	fmt.Println("array ", array)
 	fmt.Println("count ", count)
+	result := make([]int, 0, len(array))
+
+	fmt.Println("1 result ", result)
+	for _, i := range array[count:] {
+		result = append(result, i)
+	}
+	for _, i := range array[:count] {
+		result = append(result, i)
+	}
+	fmt.Println("2 result ", result)
+	// result = append(result, preArray)
+	// result = append(result, postArray)
+
+	fmt.Println("result ", result)
+	return result
 }
