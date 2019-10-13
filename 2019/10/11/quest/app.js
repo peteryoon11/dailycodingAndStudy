eb29dfc21e9cb2cb113eb756a236cf519d654d16
peteryoon11 <yjisub11@gmail.com>
2019-10-13 21:53:09 +0900

    add  quest

--- 2019/10/11/quest/app.js
+++ 2019/10/11/quest/app.js
@@ -1,6 +1,34 @@
 /**/
 
-let aArray=[1,2,3,4,5,6,7,8];
-let bArray=[1,2,3,4,5,6,7,8];
-let cArray=[1,2,3,4,5,6,7,8];
-let dArray=[1,2,3,4,5,6,7,8];
+
+let answer="YNYYNN";
+const main=()=>{
+    let aArray=[1,2,3,4,5,6,7,8];
+    let bArray=[1,2,3,4,9,10,11,12];
+    let cArray=[1,2,3,4,9,10,13,14];
+    let dArray=[1,2,3,4,9,11,13,15];
+    
+
+for(let i=0;i<16;i++){
+    if(check(aArray, i)!=answer.substring(0,1))
+    {
+        continue;
+    }
+    return i;
+}
+return 0;
+    
+}
+
+
+const check= (x, number)=>{
+    for(const item of x){
+        if(item===number){
+            return 'Y';
+        }else{
+            return 'N';
+        }
+        
+    }
+}
+console.log(main());
