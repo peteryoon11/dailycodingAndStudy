39d7bf892162ead9e64e6ec7bb87671629489436
peteryoon11 <yjisub11@gmail.com>
2020-08-14 07:10:12 +0900

    add content for today

--- tool/makeTodayFile/test/main.go
+++ tool/makeTodayFile/test/main.go
@@ -4,23 +4,28 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
 func main() {
-	fileName := "temp.txt"
-	_, err := os.Stat(fileName)
-	if os.IsNotExist(err) {
-		file, err := os.Create("temp.txt")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-	} else {
-		currentTime := time.Now().Local()
-		err = os.Chtimes(fileName, currentTime, currentTime)
-		if err != nil {
-			fmt.Println(err)
-		}
+	// fileName := "temp.txt"
+	// _, err := os.Stat(fileName)
+	// if os.IsNotExist(err) {
+	// 	file, err := os.Create("temp.txt")
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	defer file.Close()
+	// } else {
+	// 	currentTime := time.Now().Local()
+	// 	err = os.Chtimes(fileName, currentTime, currentTime)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Println(dir)
 }
